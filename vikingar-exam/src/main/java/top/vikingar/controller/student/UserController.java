15d887454ad0f384c1fe23e53333b56084d00b75
vikingar <423141759@qq.com>
2024-02-18 23:55:36 +0800

    dev

--- vikingar-exam/src/main/java/top/vikingar/controller/student/UserController.java
+++ vikingar-exam/src/main/java/top/vikingar/controller/student/UserController.java
@@ -3,15 +3,21 @@ package top.vikingar.controller.student;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
 import top.vikingar.base.BaseApiController;
 import top.vikingar.base.RestResponse;
+import top.vikingar.domain.RoleEnum;
 import top.vikingar.domain.User;
+import top.vikingar.domain.UserEventLog;
+import top.vikingar.domain.UserStatusEnum;
 import top.vikingar.service.AuthenticationService;
 import top.vikingar.service.UserService;
 import top.vikingar.viewmodel.UserRegisterVM;
 
 import javax.validation.Valid;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author vikingar
@@ -23,12 +29,18 @@ import javax.validation.Valid;
 public class UserController extends BaseApiController {
 
     private final UserService userService;
+//    private final UserEventLogService userEventLogService;
+//    private final MessageService messageService;
     private final AuthenticationService authenticationService;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Autowired
-    public UserController(UserService userService, AuthenticationService authenticationService) {
+    public UserController(UserService userService, AuthenticationService authenticationService, ApplicationEventPublisher eventPublisher) {
         this.userService = userService;
+//        this.userEventLogService = userEventLogService;
+//        this.messageService = messageService;
         this.authenticationService = authenticationService;
+        this.eventPublisher = eventPublisher;
     }
 
 
@@ -40,14 +52,24 @@ public class UserController extends BaseApiController {
 
     @PostMapping("register")
     public RestResponse register(@RequestBody @Valid UserRegisterVM userModel) {
-        User userExist = userService.getUserByName(userModel);
+        User userExist = userService.getUserByName(userModel.getUserName());
         if (null != userExist) {
             return new RestResponse(2, "user exist");
         }
         User user = modelMapper.map(userModel, User.class);
         String encodePwd = authenticationService.pwdEncode(user.getPassword());
-
-        return null;
+        user.setPassword(encodePwd);
+        user.setUserUuid(UUID.randomUUID().toString());
+        user.setRole(RoleEnum.STUDENT.getCode());
+        user.setStatus(UserStatusEnum.Enable.getCode());
+        user.setLastActiveTime(new Date());
+        user.setCreateTime(new Date());
+        user.setDeleted(false);
+        userService.save(user);
+        UserEventLog userEventLog = new UserEventLog(user.getId(), user.getUserName(), user.getRealName(), new Date());
+        userEventLog.setContent("欢迎 " + user.getUserName() + "注册考试系统");
+        eventPublisher.publishEvent(userEventLog);
+        return RestResponse.ok();
     }
 
 
