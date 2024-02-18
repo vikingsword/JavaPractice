aa60cf9b8ebe06f336ab751d0f9578e7acf32019
vikingar <423141759@qq.com>
2024-02-18 22:28:46 +0800

    config

--- vikingar-exam/src/main/java/top/vikingar/VikingarExamApplication.java
+++ vikingar-exam/src/main/java/top/vikingar/VikingarExamApplication.java
@@ -3,9 +3,12 @@ package top.vikingar;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import top.vikingar.config.SystemConfig;
 
 @SpringBootApplication
 @MapperScan("top.vikingar.mapper")
+@EnableConfigurationProperties(SystemConfig.class)
 public class VikingarExamApplication {
 
     public static void main(String[] args) {
