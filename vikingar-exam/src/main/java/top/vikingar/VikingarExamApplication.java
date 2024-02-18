2714a41387d841ac78d2bb9df3bc8da4a13f2f62
vikingar <423141759@qq.com>
2024-02-18 11:16:09 +0800

    init

---
+++ vikingar-exam/src/main/java/top/vikingar/VikingarExamApplication.java
@@ -0,0 +1,13 @@
+package top.vikingar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingarExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VikingarExamApplication.class, args);
+    }
+
+}
---
+++ vikingar-exam/src/test/java/top/vikingar/VikingarExamApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vikingar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikingarExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
