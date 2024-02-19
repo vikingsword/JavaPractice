25df4caeb09a162a17f0a62a1887145bc0b991aa
vikingar <423141759@qq.com>
2024-02-19 21:29:30 +0800

    dev

--- vikingar-exam/src/main/java/top/vikingar/content/WebContent.java
+++ vikingar-exam/src/main/java/top/vikingar/content/WebContent.java
@@ -1,7 +1,11 @@
 package top.vikingar.content;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import top.vikingar.domain.User;
 import top.vikingar.service.UserService;
 
 /**
@@ -23,7 +27,17 @@ public class WebContent {
     }
 
 
-    public void getCurrentUser() {
-
+    public User getCurrentUser() {
+        User user = (User) RequestContextHolder.currentRequestAttributes().getAttribute(USER_ATTRIBUTES, RequestAttributes.SCOPE_REQUEST);
+        if (null != user) {
+            return user;
+        } else {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            org.springframework.security.core.userdetails.User securityUser = (org.springframework.security.core.userdetails.User) principal;
+            if (securityUser == null) {
+                return null;
+            }
+            return user;
+        }
     }
 }
