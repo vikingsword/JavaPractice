3a2ad11af26c06798e834f3b393f6320f5554a66
vikingar <423141759@qq.com>
2024-06-22 15:22:25 +0800

    feat: 责任链模式 if-else

---
+++ design-pattern/design-pattern-13-01/src/test/java/top/vikingar/test/ApiTest.java
@@ -0,0 +1,42 @@
+package top.vikingar.test;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.vikingar.design.AuthController;
+import top.vikingar.design.AuthService;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * @author vikingar
+ * @time 2024/6/22 14:33
+ * @description
+ */
+public class ApiTest {
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void test_AuthController() throws ParseException {
+        AuthController authController = new AuthController();
+
+        // 模拟三级负责人审批
+        logger.info("测试结果：{}", JSON.toJSONString(authController.doAuth("小傅哥", "1000998004813441", new Date())));
+        logger.info("测试结果：{}", "模拟三级负责人审批，王工");
+        AuthService.auth("1000013", "1000998004813441");
+
+        // 模拟二级负责人审批
+        logger.info("测试结果：{}", JSON.toJSONString(authController.doAuth("小傅哥", "1000998004813441", new Date())));
+        logger.info("测试结果：{}", "模拟二级负责人审批，张经理");
+        AuthService.auth("1000012", "1000998004813441");
+
+        // 模拟一级负责人审批
+        logger.info("测试结果：{}", JSON.toJSONString(authController.doAuth("小傅哥", "1000998004813441", new Date())));
+        logger.info("测试结果：{}", "模拟一级负责人审批，段总");
+        AuthService.auth("1000011", "1000998004813441");
+
+        logger.info("测试结果：{}", "审批完成");
+    }
+}
